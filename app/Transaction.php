4cc3a2592cf8d7ed7a36acbb87334ddba9b87ad7
Andri Alpian <an.dri050695@gmail.com>
2021-04-29 04:17:58 +0700

    first commit

---
+++ app/Transaction.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Transaction extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'user_id',
+        'inscurence_price',
+        'shipping_price',
+        'transactions_status',
+        'total_price',
+        'code'
+
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id', 'id');
+    }
+}
